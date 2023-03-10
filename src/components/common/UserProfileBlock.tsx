1db78a8d6e56421a684f4761029702ed575c132c
Mark Arenz <arenz.mark@gmail.com>
2023-03-09 20:58:09 -0500

    feat(Profile): author profile page

--- src/components/common/UserProfileBlock.tsx
+++ src/components/common/UserProfileBlock.tsx
@@ -1,4 +1,7 @@
 import { UserProfile } from '@/types';
+import { useIntl } from 'react-intl';
+import ButtonIcon from './ButtonIcon';
+import IconMessages from '../icons/IconMessages';
 import Avatar from '@/components/common/avatar/Avatar';
 import UserProfileBioDisplay from './UserProfileBioDisplay';
 
@@ -6,6 +9,7 @@ type Props = {
   userProfile: UserProfile;
 };
 const UserProfileBlock: React.FC<Props> = ({ userProfile }) => {
+  const { formatMessage } = useIntl();
   const avatarData = {
     avatarPattern: userProfile.avatarPattern,
     avatarColor1r: userProfile.avatarColor1r,
@@ -21,35 +25,55 @@ const UserProfileBlock: React.FC<Props> = ({ userProfile }) => {
   return (
     <div className="w-full">
       <div className="flex gap-1">
-        <div className="h-10 w-10">
+        <div className="h-10 w-10 aspect-1 ">
           <Avatar avatarData={avatarData} displayOnly={true} />
         </div>
         <div className="flex-grow">
-          <div className="pb-1">
-            <h1 className="text-3">{userProfile?.name}</h1>
-          </div>
-          <div className="pb-1">
-            <span className="label">Bio</span>
-            <UserProfileBioDisplay bio={userProfile.bio} />
-          </div>
-          <div className="grid">
+          <div className="grid gap-1">
             <div className="grid-row grid-row-2">
-              <div className="pb-1">
-                <span className="label">Username</span>
-                {userProfile?.username}
+              <div>
+                <div className="pb-1">
+                  <h1 className="text-3 flex items-center">
+                    {userProfile?.name}
+                    <div className="ml-1">
+                      <ButtonIcon
+                        onClick={() => {}}
+                        label={formatMessage({ id: 'feed__profile_message' })}
+                      >
+                        <div className="hover-zoom h-3 w-3 py-0-5 px-0-5">
+                          <IconMessages filled={true} color="gray-6" colorDark="gray-2" />
+                        </div>
+                      </ButtonIcon>
+                    </div>
+                  </h1>
+                </div>
+                <div className="pb-1">
+                  <span className="label">Bio</span>
+                  <UserProfileBioDisplay bio={userProfile.bio} />
+                </div>
               </div>
-              <div className="pb-1">
-                <span className="label">Swatches</span>
-                {userProfile.numSwatches}
-              </div>
-            </div>
-            <div className="grid-row grid-row-2">
-              <div className="pb-1">
-                <span className="label">Score</span>
-                {userProfile.score}
-              </div>
-              <div className="pb-1">
-                <span className="label">Level</span> {userProfile.level}
+              <div className="md-pt-6">
+                <div className="grid grid-sm">
+                  <div className="grid-row grid-row-2">
+                    <div className="pb-1">
+                      <span className="label">Username</span>
+                      {userProfile?.username}
+                    </div>
+                    <div className="pb-1">
+                      <span className="label">Swatches</span>
+                      {userProfile.numSwatches}
+                    </div>
+                  </div>
+                  <div className="grid-row grid-row-2">
+                    <div className="pb-1">
+                      <span className="label">Score</span>
+                      {userProfile.score}
+                    </div>
+                    <div className="pb-1">
+                      <span className="label">Level</span> {userProfile.level}
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
